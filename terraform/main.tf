da710ca3b552b59e4d03c2dc71dde2f8bf01d535
Jakub Wolniewicz <jakub.wolniewicz@gmail.com>
2022-07-25 21:26:37 +0200

    Add terraform ubuntu vm

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+provider "proxmox" {
+    pm_api_url = "https://${var.proxmox_host}:8006/api2/json"
+    pm_tls_insecure = true
+
+    pm_api_token_id = "${var.api_token_id}"
+    pm_api_token_secret = "${var.api_token_secret}"
+}
+
+resource "proxmox_lxc" "ubuntu" {
+    vmid = 200
+    count = 1
+    target_node  = "pve"
+    hostname     = "ubuntu"
+    ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+    password     = "${var.ubuntu_password}"
+
+    ssh_public_keys = <<-EOT
+        ${var.ubuntu_ssh}
+    EOT
+
+    onboot = true
+    start = true
+
+    unprivileged = true
+
+    cores = 8
+    memory = 8192
+    swap = 512
+
+    features {
+        nesting = true
+    }
+
+    // Terraform will crash without rootfs defined
+    rootfs {
+        storage = "local-lvm"
+        size    = "32G"
+    }
+
+    network {
+        name = "eth0"
+        bridge = "vmbr0"
+        ip = "${var.ubuntu_ip}/24"
+        gw = "${var.ubuntu_gateway}"
+    }
+}
+
---
+++ terraform/vars.tf
@@ -0,0 +1,29 @@
+variable "proxmox_host" {
+    default = ""
+}
+
+variable "api_token_id" {
+    default = ""
+    sensitive=true
+}
+variable "api_token_secret" {
+    default = ""
+    sensitive=true
+}
+
+variable "ubuntu_password" {
+    default = ""
+    sensitive=true
+}
+
+variable "ubuntu_ssh" {
+    default = ""
+}
+
+variable "ubuntu_ip" {
+    default = ""
+}
+
+variable "ubuntu_gateway" {
+    default = ""
+}
